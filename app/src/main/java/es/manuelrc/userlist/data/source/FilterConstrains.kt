6b96914e93f52687e23fc4ecdc568c44e715dc3a
manuelramos <manuelramos@instapack.es>
2022-05-03 21:49:46 +0200

    refactor

--- app/src/main/java/es/manuelrc/userlist/data/source/FilterConstrains.kt
+++ app/src/main/java/es/manuelrc/userlist/data/source/FilterConstrains.kt
@@ -2,15 +2,13 @@ package es.manuelrc.userlist.data.source
 
 import android.location.Location
 
-class FilterConstrains(
+ data class FilterConstrains(
     var order: OrderedEnum,
     var isFavorite: Boolean? = null,
     var isLocation: Boolean? = null,
     var currentLocation: Location? = null,
     var query: String = ""
 ) {
-    fun copy(): FilterConstrains =
-        FilterConstrains(order, isFavorite, isLocation, currentLocation, query)
 
     enum class OrderedEnum {
         NAME,
